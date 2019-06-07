4b5d6794a40def700e3a54a47137190bd3bef83d
Yuki <>
2019-06-07 13:51:06 +0900

    跳ね返り

--- fukusyu0607/Form1.cs
+++ fukusyu0607/Form1.cs
@@ -9,12 +9,44 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace fukusyu0607
-{
+{    
+   
     public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+       
+        
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+            if (label1.Left<0)
+            {
+                vx = Math.Abs(vx);
+            }
+            if (label1.Top<0)
+            {
+                vy = Math.Abs(vy);
+            }
+            if(label1.Left>250)
+            {
+                vx = -vy;
+            }
+            if(label1.Top>250)
+            {
+                vy = -vy;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
